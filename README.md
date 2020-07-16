b19e8c7f70722aba69363358d9aceeadf4c7bf02
Arjun Khandkar <khandkararjun@gmail.com>
2020-07-16 15:49:31 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Load Tests for Nexa using Locust
+
+## Deployment
+`locust -f <file> --host <host>`
+
+## Configuration
+Session key should be updated in `testConfig.ini`
+Update blocksSynced to reflect best-synced if you wish to avoid querying blocks that may not have been synced
---
+++ transactionsByTxId.py
@@ -0,0 +1,41 @@
+import json
+import random
+import configparser
+from locust import HttpUser, TaskSet, between, task
+
+cfgParser = configparser.ConfigParser()
+cfgParser.read("testConfig.ini")
+
+blocksUpperLimit = int(cfgParser["testConfig"]["blocksSynced"])
+sessionKey = str(cfgParser["testConfig"]["sessionKey"])
+
+class TxByTxIdApiUser(HttpUser):
+    wait_time = between(2, 5)
+
+    # user fetches a random block by height, uses the block hash to fetch all txns from that block
+    def on_start(self):
+        response = self.client.get(
+            url="/v1/block/height/" + str(random.randint(1, blocksUpperLimit)),
+            headers={"authorization": "Bearer " + sessionKey},
+            verify=False
+        )
+        responseObject = json.loads(response.text)
+        self.blockHash = responseObject["block"]["hash"]
+        response = self.client.get(
+            url="/v1/block/txids/" + responseObject["block"]["hash"],
+            headers={"authorization": "Bearer " + sessionKey},
+            verify=False
+        )
+        print(response.text)
+        responseObject = json.loads(response.text)
+        self.txids = responseObject["txids"]
+
+    # user queries all transactions from the block by txid
+    @task
+    def getTxsByTxIds(self):
+        while(len(self.txids) != 0):
+            self.client.get(
+                url="/v1/transaction/" + self.txids.pop(),
+                headers={"authorization": "Bearer " + sessionKey},
+                verify=False
+            )
